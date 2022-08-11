f1e8f2cf22dce875fc3e6c594e4b7fd54214e5a0
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-08-11 15:57:36 +0200

    Deleting Tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -10,7 +10,13 @@ export class TaskService {
   private apiUrl = 'http://localhost:5000/tasks';
 
   constructor(private http: HttpClient) {}
+
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
   }
 }
